36bac189da2e38c2747722c085b67c91f3553aea
❄snow_____field❄ <86102527+asnowfield@users.noreply.github.com>
2023-06-04 23:03:36 +0900

    PPTSource

---
+++ passWordPPT/MemberEntity.java
@@ -0,0 +1,51 @@
+package com.snow.board.entity;
+
+import com.snow.board.dto.MemberDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(unique = true) // unique 제약조건 추가
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO, PasswordEncoder passwordEncoder) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+
+        String encodedPassword = passwordEncoder.encode(memberDTO.getMemberPassword());
+        memberEntity.setMemberPassword(encodedPassword);
+
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO, PasswordEncoder passwordEncoder) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+
+        String encodedPassword = passwordEncoder.encode(memberDTO.getMemberPassword());
+        memberEntity.setMemberPassword(encodedPassword);
+
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
+}
